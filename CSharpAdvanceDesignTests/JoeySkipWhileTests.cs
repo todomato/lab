4394131df48f84ca674da3d130b7150ad9708b6a
dreamrrrr <dreamrrrr@hotmail.com>
2020-02-28 17:33:51 +0800

    [test] skipwhile

--- CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipWhileTests.cs
@@ -9,11 +9,11 @@ using System.Linq;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
+    //[Ignore("not yet")]
     public class JoeySkipWhileTests
     {
         [Test]
-        public void skip_cards_until_separate_card()
+        public void skip_cards_is_normal()
         {
             var cards = new List<Card>
             {
@@ -26,7 +26,8 @@ namespace CSharpAdvanceDesignTests
                 new Card {Kind = CardKind.Separate},
             };
 
-            var actual = JoeySkipWhile(cards);
+            // 讓func語意 同測試名稱
+            var actual = JoeySkipWhile(cards, card => card.Kind == CardKind.Normal);
 
             var expected = new List<Card>
             {
@@ -36,12 +37,22 @@ namespace CSharpAdvanceDesignTests
                 new Card {Kind = CardKind.Separate},
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+            expected.ToExpectedObject().ShouldMatch(actual.ToList());
         }
 
-        private IEnumerable<Card> JoeySkipWhile(IEnumerable<Card> cards)
+        private IEnumerable<Card> JoeySkipWhile(IEnumerable<Card> cards, Func<Card, bool> predicate)
         {
-            throw new NotImplementedException();
+            var enumerator = cards.GetEnumerator();
+            var pass = false;
+            while (enumerator.MoveNext())
+            {
+                var card = enumerator.Current;
+                if (!predicate(card) || pass)
+                {
+                    pass = true;
+                    yield return enumerator.Current;
+                }
+            }
         }
     }
 }
