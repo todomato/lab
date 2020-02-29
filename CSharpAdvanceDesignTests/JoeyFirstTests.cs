97338b7bab5f2f8860d7f459cfb7e5c9b03849cd
dreamrrrr <dreamrrrr@hotmail.com>
2020-02-29 11:12:03 +0800

    [test][refactor] first *while 可以先寫,但如果一進去就return 就可以依序把while 改成 if,因為只有一次

--- CSharpAdvanceDesignTests/JoeyFirstTests.cs
+++ CSharpAdvanceDesignTests/JoeyFirstTests.cs
@@ -1,4 +1,5 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -6,7 +7,7 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
+    //[Ignore("not yet")]
     public class JoeyFirstTests
     {
         [Test]
@@ -14,20 +15,39 @@ namespace CSharpAdvanceDesignTests
         {
             var girls = new[]
             {
-                new Girl(){Age = 10},
+                new Girl(){Age = 60},
                 new Girl(){Age = 20},
                 new Girl(){Age = 30},
             };
 
             var girl = JoeyFirst(girls);
-            var expected = new Girl { Age = 10 };
+            var expected = new Girl { Age = 60 };
 
             expected.ToExpectedObject().ShouldEqual(girl);
         }
 
-        private Girl JoeyFirst(IEnumerable<Girl> girls)
+        [Test]
+        // 應該要有設計有例外狀況
+        public void get_first_girl_when_no_girls()
         {
-            throw new System.NotImplementedException();
+            var girls = new Girl[]
+            {
+            };
+
+            TestDelegate action = () => JoeyFirst(girls);
+            Assert.Throws<InvalidOperationException>(action);
+        }
+
+        private TSource JoeyFirst<TSource>(IEnumerable<TSource> girls)
+        {
+            var enumerator = girls.GetEnumerator();
+            //遇到 var return 這種是沒有意義的,爾且會有生命週期
+            //可以改用function,就不會有生命週期
+
+            // while 可以先寫,但如果一進去就return 就可以依序把while 改成 if,因為只有一次 
+            return enumerator.MoveNext()
+                ? enumerator.Current
+                : throw new InvalidOperationException($"{nameof(girls)} is empty");
         }
     }
 }
