7cb38ec8998efde584ff62c58519dcbb82f4a3e2
andy <messboy000@gmail.com>
2020-03-01 15:02:51 +0800

    [feat] JoeyIntersect

--- CSharpAdvanceDesignTests/JoeyIntersectTests.cs
+++ CSharpAdvanceDesignTests/JoeyIntersectTests.cs
@@ -5,14 +5,14 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
+    //[Ignore("not yet")]
     public class JoeyIntersectTests
     {
         [Test]
         public void intersect_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 7, 3 };
+            var first = new[] { 1, 3, 5, 3 };
+            var second = new[] { 5, 7, 3, 7};
 
             var actual = JoeyIntersect(first, second);
 
@@ -23,7 +23,18 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyIntersect(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var firstHashSet = new HashSet<int>(first);
+            var secondHashSet = new HashSet<int>(second);
+            var enumerator = firstHashSet.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var current = enumerator.Current;
+                if (!secondHashSet.Add(current))
+                {
+                    yield return current;
+                }
+                
+            }
         }
     }
 }
