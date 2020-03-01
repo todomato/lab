c7a0be8d4f73dee9725d3ea9f152a437b14f9905
andy <messboy000@gmail.com>
2020-03-01 13:58:35 +0800

    [Tests][distinct] imp IequalityComparer

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -1,7 +1,9 @@
-﻿using ExpectedObjects;
+﻿using System;
+using ExpectedObjects;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using Lab.Entities;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -20,6 +22,50 @@ namespace CSharpAdvanceDesignTests
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
+        [Test]
+        public void distinct_employees()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyDistinct(employees);
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Employee> JoeyDistinct(IEnumerable<Employee> employees)
+        {
+            return new HashSet<Employee>(employees, new FullNameComparer());
+        }
+
+        internal class FullNameComparer : IEqualityComparer<Employee>
+        {
+            public bool Equals(Employee x, Employee y)
+            {
+                return (x.FirstName == y.FirstName && x.LastName == y.LastName);
+            }
+
+            public int GetHashCode(Employee obj)
+            {
+                // 取得比對後相等的唯一值
+                // 匿名型別 也可用在測試比較類別裡面的幾個property
+                // 效能問題 等變成瓶頸再考慮
+                return new {obj.FirstName, obj.LastName}.GetHashCode();
+            }
+        }
+
         private IEnumerable<int> Distinct(IEnumerable<int> numbers)
         {
             // 自己去重複 的資料結構特性
@@ -36,5 +82,8 @@ namespace CSharpAdvanceDesignTests
             }
             return result;
         }
+
     }
+
+  
 }
