e374ffb04ac5967f3feb3fea359c79ba109d55ab
andy <messboy000@gmail.com>
2020-03-01 14:22:16 +0800

    [refactor] 序列畫 反序列畫 加減密 壓縮解壓縮 比較會消耗效能 alt insert

--- Lab/Entities/Employee.cs
+++ Lab/Entities/Employee.cs
@@ -6,5 +6,13 @@
         public string FirstName { get; set; }
         public Role Role { get; set; }
         public int Age { get; set; }
+
+        public override string ToString()
+        {
+            //序列畫 反序列畫 加減密 壓縮解壓縮 比較會消耗效能
+
+            //常用在寫 logger
+            return $"{nameof(LastName)}: {LastName}, {nameof(FirstName)}: {FirstName}, {nameof(Role)}: {Role}, {nameof(Age)}: {Age}";
+        }
     }
 }
