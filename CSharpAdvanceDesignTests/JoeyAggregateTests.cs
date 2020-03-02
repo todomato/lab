a028aa057976e4f850f4d72077e56a6d9d83f23e
andy <messboy000@gmail.com>
2020-03-02 09:13:45 +0800

    [refactor][aggreate] extract parameter, generic

--- CSharpAdvanceDesignTests/JoeyAggregateTests.cs
+++ CSharpAdvanceDesignTests/JoeyAggregateTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using System.Collections.Generic;
 
 namespace CSharpAdvanceDesignTests
@@ -18,24 +19,39 @@ namespace CSharpAdvanceDesignTests
                 30, 80, 20, 40, 25
             };
 
-            var actual = JoeyAggregate(drawlingList, balance);
+            var actual = JoeyAggregate(drawlingList, 
+                balance,
+                (total, next) => total - next > 0 ? total - next : total);
 
             var expected = 10.91m;
 
             Assert.AreEqual(expected, actual);
         }
 
-        private decimal JoeyAggregate(IEnumerable<int> drawlingList, decimal balance)
+        [Test]
+        public void Determine_whether_any_string_in_the_array_is_longer_than_banana_()
         {
-            var drawEnumerator = drawlingList.GetEnumerator();
-            decimal result = balance;
-            while (drawEnumerator.MoveNext())
+            // test
+            string[] fruits = { "apple", "mango", "orange", "passionfruit", "grape" };
+            var banana = "banana";
+
+            var actual = JoeyAggregate(
+                fruits,
+                banana,
+                (longest, next) => (longest.Length > next.Length) ? longest: next);
+
+            var expected = "passionfruit";
+            Assert.AreEqual(expected, actual);
+        }
+
+        private TAccumate JoeyAggregate<TSource,TAccumate>(IEnumerable<TSource> source, TAccumate seed, Func<TAccumate, TSource, TAccumate> func)
+        {
+            var enumerator = source.GetEnumerator();
+            var result = seed;
+            while (enumerator.MoveNext())
             {
-                var current = drawEnumerator.Current;
-                if (result - current > 0)
-                {
-                    result -= current;
-                }
+                var current = enumerator.Current;
+                result = func(result, current);
             }
             return result;
         }
